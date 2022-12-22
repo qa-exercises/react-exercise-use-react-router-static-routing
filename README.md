9648f292d851eded7515c257e48c9fedbd84ecfd
qa-exercises <118732445+qa-exercises@users.noreply.github.com>
2022-12-22 01:34:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-exercise-use-react-router-static-routing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-exercise-use-react-router-static-routing)
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>React App</h1>
+      <ul>
+        <li>
+          <p>Home</p>
+        </li>
+        <li>
+          <p>About</p>
+        </li>
+      </ul>
+    </nav>
+  )
+}
